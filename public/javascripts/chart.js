9caffa5442cc6bee91cdd6c8ee8b620765e0739f
Mariot Chauvin <mariot.chauvin@gmail.com>
2012-03-16 16:13:16 +0100

    Integrate graph timeline
    Display a graph timeline with comet and raphaeljs.

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,10 +1,10 @@
-function Chart(width, height)  {
+function Chart(id, width, height)  {
 	
 
-    this.barWidth = 10;
+    this.barWidth = 7;
     this.barWidthOffset = 5;
 
-	this.paper = Raphael(0, 0, width, height);
+	this.paper = Raphael(id, width, height);
 
 	this.frame = function() {
 		this.paper.rect(0, 0, width, height);
@@ -14,21 +14,48 @@ function Chart(width, height)  {
         this.paper.rect(0, height/2, width, 1);        
     };
 
-    this.positive_bar = function (index, percent) {
+
+    this.popup = function (bar, text) {
+        var paper = this.paper;
+        
+        var txt;
+
+        bar.hover(
+            function () {
+                txt = paper.text(bar.attr("x") + 6 , bar.attr("y") , text);  
+            },
+            function () {
+                txt.remove();
+            }
+        );
+    }
+
+    this.neutral_bar = function (index, percent, num_ne) {
+        var barHeight = ((percent/100) * (height/2));
+        var bar = this.paper.rect((index * this.barWidth) + (index* this.barWidthOffset), height/2 - barHeight/2, this.barWidth, barHeight);
+        bar.attr("fill", "#E78D41");
+        bar.attr("stroke-width", "0");
+    }
+
+    this.positive_bar = function (index, percent, num_po) {
         var barHeight = ((percent/100) * (height/2));
         var bar = this.paper.rect((index * this.barWidth) + (index* this.barWidthOffset), height/2 - barHeight, this.barWidth, barHeight);
         bar.attr("fill", "#92d139");
+        bar.attr("stroke-width", "0");
+        this.popup(bar, num_po + " Tweets");
     }
 
-    this.negative_bar = function (index, percent) {
+    this.negative_bar = function (index, percent, num_ng) {
         var barHeight = ((percent/100) * (height/2));
         var bar = this.paper.rect((index * this.barWidth) + (index * this.barWidthOffset), height/2, this.barWidth, barHeight);
         bar.attr("fill", "#FA5858");
+        bar.attr("stroke-width", "0");
     }
 
-    this.bar = function(index, positive, negative) {
-        this.positive_bar(index, positive);
-        this.negative_bar(index, negative);
+    this.bar = function(index, neutral, positive, negative, num_ne, num_po, num_ng) {
+        this.positive_bar(index, positive, num_po);
+        this.negative_bar(index, negative, num_ng);
+        this.neutral_bar(index, neutral, num_ne);
     }
 
 
