8b4baad9eb6974be2c0b994f4f1dd7d2266e07be
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2014-01-02 13:31:51 +0100

    Async Sentiment service

--- app/services/Sentiment.scala
+++ app/services/Sentiment.scala
@@ -3,13 +3,14 @@ package services
 import models._
 import play.api.libs.ws.WS
 import play.api.libs.json.Json._
-import play.api.libs.concurrent.Promise
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object Sentiment {
   /**
    * Get opinion
    */
-  def getSentiment(text: String):Promise[Opinion] = {
+  def getSentiment(text: String): Future[Opinion] = {
     WS.url(sentimentUrl).withQueryString(("api_key", key), ("text", text))
       .get()
       .map(_.json.as[Opinion])
@@ -17,19 +18,17 @@ object Sentiment {
   /**
    * Add new sentence to repo
    */
-  def train(text: String, mood: String): Boolean = {
+  def train(text: String, mood: String): Future[Boolean] = {
     WS.url(trainUrl).withQueryString(("api_key", key), ("text", text), ("mood", mood))
       .get()
-      .map { response => (response.json \ "status").as[String] }
-      .value.get == "ok"
+      .map { response => (response.json \ "status").as[String] == "ok" }
   }
   /**
    * Check quota
    */
-  def checkQuota(): Long = {
+  def checkQuota(): Future[Long] = {
     WS.url(quotaUrl).withQueryString(("api_key", key))
       .get()
       .map { response => (response.json \ "quota_remaining").as[Long] }
-      .value.get;
   }
 }
