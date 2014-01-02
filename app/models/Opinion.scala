911c281fa250af74a16d03184b8d68f257fa22f2
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2014-01-02 13:23:11 +0100

    Update Opnion json read/write

--- app/models/Opinion.scala
+++ app/models/Opinion.scala
@@ -1,31 +1,12 @@
 package models
 
-import com.novus.salat._
-import com.novus.salat.global._
-import com.novus.salat.annotations._
-import com.novus.salat.dao._
-import com.mongodb.casbah.Imports._
-import com.mongodb.casbah.MongoConnection
-import play.modules.mongodb._
 import play.api.libs.json._
 import play.api.Play.current
-import java.util.Date
 
 case class Opinion(mood: String, prob: Double) {}
 
-//case class Tweet(message:String, source:String, hashs:List[String])
-
 object Opinion {
-
-  implicit object OpinionFormat extends Format[Opinion] {
-    def reads(json: JsValue): Opinion = Opinion(
-      (json \ "mood").as[String],
-      (json \ "prob").as[Double])
-
-    def writes(o: Opinion): JsValue = JsObject(Seq(
-      "mood" -> JsString(o.mood),
-      "prob" -> JsNumber(o.prob)
-    ))
-  }
+  implicit val opinionRead = Json.reads[Opinion]
+  implicit val opinionWrite = Json.writes[Opinion]
 }
 
