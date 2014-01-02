a4fee3042b93c48a99bd33585535a482f734eec8
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2014-01-02 13:05:57 +0100

    Update profile worker

--- app/actors/ProfileWorker.scala
+++ app/actors/ProfileWorker.scala
@@ -1,55 +1,37 @@
 package actors
 
-
 import play.api._
 import play.api.libs.iteratee._
-import play.api.libs.iteratee.Enumerator.Pushee
 import play.Logger
 
 import akka.actor._
 import akka.actor.Actor._
-import play.api.libs.concurrent._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.Play.current
 import play.libs.Akka
-import models.{Tweet, Opinion}
-
+import models.{ Tweet, Opinion }
 
 class ProfileWorker extends Actor {
-   import ProfileWorker._
-   var listeners : List[(String, Pushee[Tweet])] = Nil
 
-   def receive = {
-      case Listen(term) => {
-         lazy val channel: Enumerator[Tweet] = Enumerator.pushee(
-            pushee => self ! Init(pushee, term),
-            onComplete = self ! Quit()
-         )
-         Logger.info("New opinion stream on")
-         sender ! channel
-      }
-      case Init(pushee,term) => {
-         listeners = listeners :+ (term -> pushee)
-      }
+  import ProfileWorker._
+  val (out, channel) = Concurrent.broadcast[(String, Tweet)]
+  def receive = {
 
-      case Quit() => {
-         Logger.info("Opinion stream stopped ...")
+    case Listen(term) =>
+      val termaTee: Enumeratee[(String, Tweet), Tweet] = Enumeratee.collect {
+        case (t, tw) if t == term => tw
       }
+      sender ! TweetStream(out &> termaTee)
 
-      case NewTweets(term, tweets @ _*) => {
-         Logger.debug("Will stream "+tweets.size + " tweets with opinion")
-         listeners.filter(_._1 == term).foreach(p => tweets.foreach( t => {
-           p._2.push(t)}))
-      }
-      case _ => {
-         Logger.info("error matching actor message")
-      }
-   }
+    case NewTweets(term, tweets @ _*) =>
+      tweets.foreach(t => channel push (term, t))
+
+  }
 }
 object ProfileWorker {
-   trait Event
-   case class Listen(term:String) extends Event
-   case class Quit() extends Event
-   case class NewTweets(term:String ,o:Tweet*)
-   case class Init(p:Pushee[Tweet], term:String)
-   lazy val ref = Akka.system.actorOf(Props[ProfileWorker])
+  trait Event
+  case class Listen(term: String) extends Event
+  case class NewTweets(term: String, o: Tweet*)
+  case class TweetStream(out: Enumerator[Tweet])
+  lazy val ref = Akka.system.actorOf(Props[ProfileWorker])
 }
