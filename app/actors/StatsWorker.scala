3361115b98d3fdc271fe3fd389ccd35c998a2082
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2014-01-02 13:11:23 +0100

    Update stats worker

--- app/actors/StatsWorker.scala
+++ app/actors/StatsWorker.scala
@@ -8,34 +8,28 @@ import play.libs.Akka
 import services.Statistics
 import play.api.Logger
 import actors.StatsWorker._
-import models.{Profile, Tweet}
-
-/**
- * User: alabbe
- * Date: 30/03/12
- * Time: 15:53
- */
+import models.{ Profile, Tweet }
 
 class StatsWorker extends Actor {
-   def receive = {
-      case g:GlobalComputation => {
-         Profile.all.map(Tweet.incMapReduce(_))
-         Logger.debug("Global stats computation hook over")
-      }
-      case d:DailyComputation => {
-         //Logger.debug("Global stats computation hook over")
-      }
-      case w:WeeklyComputation => {
-         //Logger.debug("Global stats computation hook over")
-      }
-      case _ => throw new MatchError("Nothing matched")
-   }
+  def receive = {
+    case g: GlobalComputation => {
+      Profile.all.map(Tweet.incMapReduce(_))
+      Logger.debug("Global stats computation hook over")
+    }
+    case d: DailyComputation => {
+      //Logger.debug("Global stats computation hook over")
+    }
+    case w: WeeklyComputation => {
+      //Logger.debug("Global stats computation hook over")
+    }
+    case _ => throw new MatchError("Nothing matched")
+  }
 }
 
 object StatsWorker {
-   lazy val ref = Akka.system.actorOf(Props[StatsWorker])
-   sealed trait Event
-   case class GlobalComputation() extends Event
-   case class DailyComputation() extends Event
-   case class WeeklyComputation() extends Event
+  lazy val ref = Akka.system.actorOf(Props[StatsWorker])
+  sealed trait Event
+  case class GlobalComputation() extends Event
+  case class DailyComputation() extends Event
+  case class WeeklyComputation() extends Event
 }
