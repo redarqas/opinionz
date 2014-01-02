6f0e9afb7d63342223ff788d7a09971c9f62ed20
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2014-01-02 12:59:43 +0100

    Update global setting

--- app/Global.scala
+++ app/Global.scala
@@ -1,35 +1,25 @@
-/**
- * User: alabbe
- * Date: 30/03/12
- * Time: 16:02
- */
-
 import actors.StatsWorker
 import actors.StatsWorker._
-import java.util.Date
-import models._
 import play.api._
-
-import anorm._
 import scala.actors.Scheduler
-import akka.util.duration._
 import play.libs.Akka
+import scala.concurrent.duration._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import scala.language.postfixOps
 
 object Global extends GlobalSettings {
+  override def onStart(app: Application) {
 
-   override def onStart(app: Application) {
+    //incremental statistics computation
+    Akka.system.scheduler.schedule(3 seconds, 60 seconds) {
+      StatsWorker.ref ! GlobalComputation()
+    }
+    Akka.system.scheduler.schedule(3 seconds, 1 day) {
+      StatsWorker.ref ! DailyComputation()
+    }
+    Akka.system.scheduler.schedule(3 seconds, 7 days) {
+      StatsWorker.ref ! WeeklyComputation()
+    }
 
-      //incremental statistics computation
-      Akka.system.scheduler.schedule(3 seconds, 60 seconds) {
-         StatsWorker.ref ! GlobalComputation()
-      }
-      Akka.system.scheduler.schedule(3 seconds, 1 day) {
-         StatsWorker.ref ! DailyComputation()
-      }
-      Akka.system.scheduler.schedule(3 seconds, 7 days ) {
-         StatsWorker.ref ! WeeklyComputation()
-      }
-
-
-   }
+  }
 }
